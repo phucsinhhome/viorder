fadf538fe1820e25df58ae4196dc8641a274d969
Minh Trần <wisekingtran@gmail.com>
2023-12-14 21:45:35 +0700

    Update ProfitReport.js

--- src/Components/Profit/ProfitReport.js
+++ src/Components/Profit/ProfitReport.js
@@ -16,6 +16,33 @@ const reports = [
     key: "investors"
   }
 ]
+const lastDateOf = (date) => {
+  return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+}
+
+const beginOfDay = (date) => {
+  date.setHours(0)
+  date.setMinutes(0)
+  date.setSeconds(0)
+  return date
+}
+
+const endOfDay = (date) => {
+  date.setHours(23)
+  date.setMinutes(59)
+  date.setSeconds(59)
+  return date
+}
+
+const adjustMonths = (date, numOfMonths) => {
+  return new Date(date.setMonth(date.getMonth() + numOfMonths))
+}
+
+const beginOfMonth = (date) => {
+  date.setDate(1)
+  beginOfDay(date)
+  return date
+}
 
 export function ProfitReport() {
   const [report, setReport] = useState({
@@ -40,49 +67,126 @@ export function ProfitReport() {
     ]
   })
 
-  const beginOfMonth = () => {
-    var today = new Date()
-    today.setDate(2)
-    today.setHours(0)
-    today.setMinutes(0)
-    today.setSeconds(0)
-
-    return today
-  }
-
-  const endOfToday = () => {
-    var today = new Date()
-    today.setHours(23)
-    today.setMinutes(59)
-    today.setSeconds(59)
-
-    return today
-  }
-
   const [reportType, setReportType] = useState(reports[0])
-  const [period, setPeriod] = useState({ fromDate: beginOfMonth(), toDate: endOfToday() })
+  const [period, setPeriod] = useState(
+    {
+      name: "Today",
+      range: {
+        fromDate: beginOfMonth(new Date()),
+        toDate: endOfDay(new Date())
+      }
+    })
 
-  useEffect(() => {
+  // const timeFilters = [
+  //   {
+  //     name: "Last Month",
+  //     range: () => {
+  //       return ({
+  //         fromDate: adjustMonths(period.range.fromDate, -1),
+  //         toDate: endOf(lastDateOf(adjustMonths(period.range.toDate, -1)))
+  //       })
+  //     }
+  //   },
+  //   {
+  //     name: "Today",
+  //     range: () => {
+  //       return ({
+  //         fromDate: beginOfMonth(new Date()),
+  //         toDate: endOf(new Date())
+  //       })
+  //     }
+  //   },
+  //   {
+  //     name: "End of Month",
+  //     range: () => {
+  //       return ({
+  //         fromDate: beginOfMonth(new Date()),
+  //         toDate: endOf(lastDateOf(new Date()))
+  //       })
+  //     }
+  //   },
+  //   {
+  //     name: "Next Month",
+  //     range: () => {
+  //       return ({
+  //         fromDate: adjustMonths(period.range.fromDate, 1),
+  //         toDate: endOf(adjustMonths(period.range.toDate, 1))
+  //       })
+  //     }
+  //   }
+  // ]
 
-    var fD = period.fromDate.toISOString().split('T')[0]
-    var tD = period.toDate.toISOString().split('T')[0]
+  const timeFilters = [
+    {
+      name: "Last Month",
+      adjustedMonths: -1,
+      entiredMonth: true
+    },
+    {
+      name: "Today",
+      adjustedMonths: 0,
+      entiredMonth: false
+    },
+    {
+      name: "End of Month",
+      adjustedMonths: 0,
+      entiredMonth: true
+    },
+    {
+      name: "Next Month",
+      adjustedMonths: 1,
+      entiredMonth: true
+    }
+  ]
+
+  const calculatePeriod = (timeFilter) => {
+
+    var nextFromDate = timeFilter.adjustedMonths === 0 ? beginOfDay(beginOfMonth(new Date())) : beginOfDay(adjustMonths(period.range.fromDate, timeFilter.adjustedMonths))
+    var nextToDate = endOfDay(new Date(nextFromDate))
+    var dayOfMonth = timeFilter.entiredMonth ? lastDateOf(nextToDate) : new Date().getDate()
+    console.info("Setting day of [%s] is [%d]", nextToDate, dayOfMonth)
+    nextToDate.setDate(dayOfMonth)
+
+    var newPeriod = {
+      name: timeFilter.name,
+      range: {
+        fromDate: nextFromDate,
+        toDate: nextToDate
+      }
+    }
+    setPeriod(newPeriod)
+  }
+
+
+  const fetchReport = () => {
+    var fD = period.range.fromDate.toISOString().split('T')[0]
+    var tD = period.range.toDate.toISOString().split('T')[0]
 
     getProfitReportThisMonth(fD, tD, reportType.key)
       .then(data => setReport(data))
-  }, [period, reportType.key]);
+  }
 
-  const filterClass = (reportKey) => {
+  useEffect(() => fetchReport(), [reportType, period]);
+
+  const filterClass = (reportKey, currentKey) => {
     var classNamePattern = "font-bold text-amber-800 rounded px-2 py-1"
-    return classNamePattern + " " + (reportType.key === reportKey ? "bg-slate-400" : "bg-slate-200");
+    return classNamePattern + " " + (currentKey === reportKey ? "bg-slate-400" : "bg-slate-200");
   }
 
   return (
     <div class="bg-slate-50">
-      <div class="flex flex-wrap py-2 px-2 space-x-4 space-y-2 ">
-        <Link onClick={() => getProfitReportThisMonth().then(data => setReport(data))} className="font-bold text-amber-800">Update</Link>
+      <div class="flex flex-wrap py-2 px-2 space-x-4 ">
+        <Link onClick={() => fetchReport()} className="font-bold text-amber-800">Update</Link>
         <div className="space-x-2">
           {reports.map((rp) => {
-            return (<Link key={rp.key} onClick={() => setReportType(rp)} relative="route" className={filterClass(rp.key)}>{rp.name}</Link>)
+            return (<Link key={rp.key} onClick={() => setReportType(rp)} relative="route" className={filterClass(rp.key, reportType.key)}>{rp.name}</Link>)
+          })}
+        </div>
+      </div>
+      <div class="flex flex-wrap py-2 px-2 space-x-4 ">
+        <div className="space-x-2">
+          {timeFilters.map((per) => {
+            return (<Link key={per.name} onClick={() => calculatePeriod(per)} relative="route" className={filterClass(per.name, period.name)}>{per.name}</Link>)
           })}
         </div>
       </div>
