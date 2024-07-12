3e75f6573c5bf8796f4a03523d68e5927055093e
Minh Trần <wisekingtran@gmail.com>
2024-07-13 00:05:33 +0700

    Add expense message form

--- src/Components/Expense/EditExpense.js
+++ src/Components/Expense/EditExpense.js
@@ -183,6 +183,23 @@ export const EditExpense = () => {
           className="px-1 font-sans font-bold text-amber-800"
         >Back</Link>
       </div>
+      <div>
+        
+<form>
+    <div class="grid gap-6 mb-6 md:grid-cols-2">
+        <div>
+            <label for="expense_message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your expense message</label>
+            <input type="text" id="expense_message" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="5kg sugar 450k" required />
+        
+        </div>
+        <div>
+            <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white invisible">G</label>
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Generate</button>
+        </div>
+    </div>
+</form>
+
+      </div>
       <form className="flex flex-wrap mx-1">
         <div className="w-full px-1 mb-6">
           <div className="flex flex-wrap -mx-3 mb-6">
