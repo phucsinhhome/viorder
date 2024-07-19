1c89c3cc1c08a0ca4f1c4af31ef5123977b1d3ed
Minh Trần <wisekingtran@gmail.com>
2024-07-19 13:34:16 +0700

    Layout item edit

--- src/Components/Invoice/EditItem.js
+++ src/Components/Invoice/EditItem.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Modal, TextInput, Label, Checkbox, Button } from 'flowbite-react';
 import { classifyServiceByItemName } from "../../Service/ItemClassificationService"
-import { HiOutlineCash, HiOutlinePlus } from "react-icons/hi";
+import { HiMinus, HiOutlineCash, HiOutlinePlus, HiPlay, HiPlus } from "react-icons/hi";
 import { Link } from "react-router-dom";
 
 
@@ -145,23 +145,25 @@ export const EditItem = ({ eItem, onSave, onDelete, displayName }) => {
                 className="w-full"
               />
             </div>
-            <div>
-              <div className="mb-2 block">
+            <div className="flex flex-row w-full align-middle">
+              <div className="flex items-center w-2/5">
                 <Label
                   htmlFor="quantity"
                   value="Quantity"
                 />
               </div>
-              <div className="flex">
-                <TextInput
-                  id="quantity"
-                  type="number"
-                  placeholder="1"
-                  required={true}
-                  value={item.quantity}
-                  onChange={onValueChange}
-                /><Button className="ml-3 bg-slate-200 text-black">+</Button>
-                <Button className="ml-3 bg-slate-200 text-black">-</Button>
+              <div class="relative flex items-center w-full">
+                <button type="button" id="decrement-button" data-input-counter-decrement="quantity-input" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                  <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
+                  </svg>
+                </button>
+                <input type="text" id="quantity-input" data-input-counter aria-describedby="helper-text-explanation" class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="999" required />
+                <button type="button" id="increment-button" data-input-counter-increment="quantity-input" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                  <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
+                  </svg>
+                </button>
               </div>
             </div>
             <div>
