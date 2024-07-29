1bd68a3b0050442d2234119a7ce31759dc79427c
Minh Trần <wisekingtran@gmail.com>
2024-07-29 21:28:37 +0700

    Update ExpenseManager.js

--- src/Components/Expense/ExpenseManager.js
+++ src/Components/Expense/ExpenseManager.js
@@ -368,13 +368,12 @@ export const ExpenseManager = () => {
           >
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5" />
           </svg>
-          <Link
+          <span
             onClick={() => editExpense(defaultEmptExpense)}
-            state={{ pageNumber: pagination.pageNumber, pageSize: pagination.pageSize }}
             className="font-bold text-amber-800"
           >
             Add Expense
-          </Link>
+          </span>
         </div>
       </div>
       <div>
